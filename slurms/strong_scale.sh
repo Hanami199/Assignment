c5a75b53dde93dd395a76c428fb06403d062a2e7
Sara Trabucco <strabuc1@login02.leonardo.local>
2025-09-10 23:22:27 +0200

    weak scaling results from Leonardo

--- slurms/strong_scale.sh
+++ slurms/strong_scale.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=8
+#!/bin/sh
+#SBATCH --nodes=16
+#SBATCH --tasks=128
+#SBATCH --cpus-per-task=14
 #SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=14          
 #SBATCH --mem=0
-#SBATCH --partition=EPYC
+#SBATCH --partition dcgp_usr_prod
+#SBATCH -A uTS25_Tornator_0
 #SBATCH -t 00:15:00
-#SBATCH --job-name=HPC_Exam
-#SBATCH --exclusive                   
-
-module load openMPI/5.0.5 
+#SBATCH --job-name=test
+#SBATCH --exclusive                                                                   
+# ======================================================= 
+module load openmpi/4.1.6--gcc--12.2.0
 
 # compile
 mpicc -march=native -O3 -std=c17 -fopenmp -Iinclude src/stencil_template_parallel.c -o stencil_parallel
@@ -18,19 +19,18 @@ mpicc -march=native -O3 -std=c17 -fopenmp -Iinclude src/stencil_template_paralle
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close # try close later?
 export OMP_DISPLAY_AFFINITY=TRUE
+export OMP_NUM_THREADS=14
 
-mkdir -p outputs_strong_scale_leo
-RESULTS="outputs_strong_scale_leo/results_1_node.csv"
+mkdir -p outputs_weak_scale_leo
+RESULTS="outputs_weak_scale_leo/results_${SLURM_JOB_NUM_NODES}Node_${SLURM_NTASKS}Tasks_${OMP_NUM_THREADS}Threads.csv"
 echo "timestamp,threads,elapsed_s,maxrss_kb" > "$RESULTS"
 
-
- export OMP_NUM_THREADS=14
  echo "Running with 14 threads"
  ts=$(date +"%Y%m%d_%H%M%S")
- log="outputs_strong_scale_leo/output_14_8Task_1Node.log"
+ log="outputs_weak_scale_leo/output_${SLURM_JOB_NUM_NODES}Node_${SLURM_NTASKS}Tasks_${OMP_NUM_THREADS}Threads_weak.log"
  # time the step and append to CSV:
  /usr/bin/time -f "%e,%M" -o /tmp/time.$$ \
-    srun --ntasks=32 --cpus-per-task=16 --cpu-bind=cores ./stencil_parallel -x 15000 -y 15000 -o 0 -v 1 > "$log"
+    srun --ntasks=${SLURM_NTASKS} --cpus-per-task=${OMP_NUM_THREADS} --cpu-bind=cores ./stencil_parallel -x 80000 -y 80000 -o 0 -v 1 > "$log"
  read elapsed_kb < /tmp/time.$$
  elapsed=${elapsed_kb%%,*}
  maxrss=${elapsed_kb##*,}
--- slurms/test1.sh
+++ slurms/test1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4          
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=14          
 #SBATCH --hint=nomultithread
 #SBATCH --mem=0
 #SBATCH --partition=GENOA
@@ -21,17 +21,17 @@ export OMP_PROC_BIND=close # try close later?
 export OMP_DISPLAY_AFFINITY=TRUE
 
 mkdir -p outputs
-RESULTS="outputs/results_${SLURM_JOB_ID}.csv"
+RESULTS="outputs/results_timetest.csv"
 echo "timestamp,threads,elapsed_s,maxrss_kb" > "$RESULTS"
 
-for nt in 1 2 4; do
-  export OMP_NUM_THREADS=$nt
-  echo "Running with $nt threads"
-  ts=$(date +"%Y%m%d_%H%M%S")
-  log="outputs/output_${ts}_1Task_${nt}Threads.log"
-  # time the step and append to CSV:
+
+export OMP_NUM_THREADS=14
+echo "Running with 14 threads"
+ts=$(date +"%Y%m%d_%H%M%S")
+log="outputs/times.log"
+# time the step and append to CSV:
   /usr/bin/time -f "%e,%M" -o /tmp/time.$$ \
-    srun --ntasks=1 --cpus-per-task=$nt --cpu-bind=cores ./stencil_parallel -e 300 -o 0 -v 1 > "$log"
+    srun --ntasks=8 --cpus-per-task=14 --cpu-bind=cores ./stencil_parallel -e 300 -o 0 -v 1 > "$log"
   read elapsed_kb < /tmp/time.$$
   elapsed=${elapsed_kb%%,*}
   maxrss=${elapsed_kb##*,}
